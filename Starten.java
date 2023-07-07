9fee424841c31f1f54707ea77a2d3538cbf9f9c1
Max <Max>
2023-07-07 11:44:11 +0200

    add Starten.java

---
+++ Starten.java
@@ -0,0 +1,20 @@
+
+/**
+ * @author Kevin
+ * @version 07.07.2023
+ */
+public class Starten
+{
+    
+    /**
+     * Klasse die das Spiel startet indem sie eine neue Main Klasse erstellt.
+     */
+    // Konstruktor
+    public Starten()
+    {
+       new Main();
+    }
+
+    // Dienste
+
+}
